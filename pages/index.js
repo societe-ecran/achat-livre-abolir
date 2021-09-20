f4d6b4135c9448b94caa8541d2778aa92f03eeed
societe-ecran <antoninreigneaud@gmail.com>
2021-09-20 20:32:50 +0200

    seo

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,23 @@ export default function Home() {
         <div>
             <html lang="fr" />
             <Head>
+            <title>{props.title}</title>
+                <meta name="description" content="sortie septembre 2021" />
+                <meta property="og:title" content="Abolir la police. Echos des états-unis." />
+                <meta property="og:description" content={props.description} />
+                <meta property="og:url" content="https://www.abolirlapolice.org/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content={`https://achat-livre-abolir.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fcouv.93478b6c9ee7db2c15aef8dc9bfe9289.jpg&w=1920&q=100`} />
+                <meta charSet="utf-8" />
+                <meta name="author" content="{Team17}" />
+                <meta name="twitter:title" content="collectifmatsu1" />
+                <meta name="theme-color" content="#59577c" />
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:site" content="@CollectifMatsu1" />
+                <meta name="twitter:title" content="Abolir la police" />
+                <meta name="twitter:description" content="Le site vise à diffuser les idées, pratiques et luttes pour l’abolition du système pénal (police, justice, prison) auprès des francophones." />
+                <meta name="twitter:image" content={`https://www.abolirlapolice.org/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2FcouvMin.64099324a2cd6af6ed4653b63dca91f1.jpg&w=256&q=50`} />
+                <meta name="twitter:image:alt" content="couverture" />
                 <link rel="preconnect" href="<https://app.snipcart.com>" />
                 <link rel="preconnect" href="<https://cdn.snipcart.com>" />
                 <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.css" />
