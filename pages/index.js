fdf43bf53df52dc1a7a4f19fb73e730cc45514f1
societe-ecran <antoninreigneaud@gmail.com>
2022-03-23 13:22:17 +0100

    disable snipcart

--- pages/index.js
+++ pages/index.js
@@ -124,16 +124,21 @@ export default function Home() {
                         </div>
             
                         <div className="text-center md:text-left pt-6 pb-6">
-                            <button className="snipcart-add-item rounded-full py-2 px-4 violetBack text-white  "
-                                data-item-id="livre"
-                                data-item-price="14"
-                                data-item-weight='494'
-                                data-item-url="/"
-                                data-item-description="Paru le 10 septembre 2021. 336 pages.  ISBN: 979-10-96195-14-5 "
+                        {/* snipcart-add-item  */}
+                        <a href="https://www.placedeslibraires.fr/listeliv.php?base=allbooks&mots_recherche=abolir+la+police" target="_blank">
+                            <button className="rounded-full py-2 px-4 violetBack text-white"
+                    
+                                // data-item-id="livre"
+                                // data-item-price="14"
+                                // data-item-weight='494'
+                                // data-item-url="/"
+                                // data-item-description="Paru le 10 septembre 2021. 336 pages.  ISBN: 979-10-96195-14-5 "
                                 // data-item-image="../public/couv.jpg"
-                                data-item-name="Abolir la police. Échos des États-Unis">
+                                // data-item-name="Abolir la police. Échos des États-Unis"
+                                >
                                 Acheter
                             </button>
+                            </a>
                         </div>
                     </div>
                 </div>
